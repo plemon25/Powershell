0f910c08050c1f9e4944df7f2e6c0dc9f93a7510
plemon25 <42594725+plemon25@users.noreply.github.com>
2021-11-09 15:03:38 -0800

    ps1 format

---
+++ ExchangeTools/New-RoomCalendar.ps1
@@ -0,0 +1,55 @@
+#Add step to check if connected to Exchange Online
+#Use this process for Shared Calendars without a UCInetID
+
+#Set Variables here
+$RoomName         = "Conference Room"
+$RoomOUPath       = "OU=Shared Mailboxes,OU=Department,OU=General,OU=Managed,DC=ad,DC=xxx,DC=com"
+$RoomID           = "ConfRoom"
+$RoomGroupName    = "ConfRoom(FullSendAs)"
+$RoomGroupAlias   = "ConfRoom-FullSendAs"
+$RoomGroupPath    = "OU=Resource Groups,OU=Groups,OU=Department,OU=General,OU=Managed,DC=ad,DC=xxx,DC=com"
+$RoomGroupOwner   = "MailEnabledGroupOwners"
+
+#Parameters for Room ADObject creation
+$NewRoomParams = @{
+    GivenName         = $RoomName
+    DisplayName       = $RoomName
+    Enabled           = $false
+    UserPrincipalName = ($RoomID + "@ad.uci.edu")
+    Verbose           = $true
+}
+
+#Create AD User Object 
+New-ADUser @NewRoomParams 
+
+#Parameters for the Remote Mailbox creation
+$EnableRemoteMailboxParams = @{
+    Identity              = $RoomID
+    RemoteRoutingAddress  = ($RoomID + "@ucirvine.mail.onmicrosoft.com")
+    Room                  = $true
+    Verbose               = $true
+}
+
+#Enable the remote mailbox in exchange online
+Enable-RemoteMailbox @EnableRemoteMailboxParams
+
+$NewRoomGroupParams = @{
+    Name            = $RoomGroupName
+    DisplayName     = $RoomGroupName
+    GroupCategory   = "Security"
+    GroupScope      = "Universal"
+    SamAccountName  = $RoomGroupName
+    Verbose         = $true
+}
+New-ADGroup @NewRoomGroupParams
+
+$NewDistributionGroupParams = @{
+    Name                = $RoomGroupName
+    DisplayName         = $RoomGroupName
+    Type                = "Security"
+    OrganizationalUnit  = $RoomGroupPath
+    Alias               = $RoomGroupAlias
+    ManagedBy           = $RoomGroupOwner
+}
+
+New-DistributionGroup @NewDistributionGroupParams
